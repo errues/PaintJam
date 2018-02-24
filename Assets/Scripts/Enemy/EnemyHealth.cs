c068210acf9e0fe9c5ba038e04f02da6547ea239
errues <errues@gmail.com>
2018-02-24 15:31:41 +0100

    Ajuste de color en inspector
    Creado enum de colores y enemigo de prueba (con preciosos sprite). El color de los enemigos se puede seleccionar en el inspector.

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+	public BasicColors color;
+
+	public SpriteRenderer filling;
+
+	private void OnValidate(){
+		SetColor ();
+	}
+
+	private void SetColor(){
+		switch (color){
+		case BasicColors.AMARILLO:
+			filling.color = Color.yellow;
+			break;
+		case BasicColors.ROJO:
+			filling.color = Color.red;
+			break;
+		case BasicColors.AZUL:
+			filling.color = Color.blue;
+			break;
+		case BasicColors.NARANJA:
+			filling.color = new Color (1F, 0.5f, 0);
+			break;
+		case BasicColors.MORADO:
+			filling.color = new Color (0.5f, 0, 1f);
+			break;
+		case BasicColors.VERDE:
+			filling.color = Color.green;
+			break;
+		case BasicColors.NEGRO:
+			filling.color = Color.black;
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/Enumerates/BasicColors.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BasicColors {
+	AMARILLO, ROJO, AZUL, NARANJA, VERDE, MORADO, NEGRO
+}

8122a24fd6acaf2f3ffa45d7964557b9c03a7574
errues <errues@gmail.com>
2018-02-24 21:01:05 +0100

    Persecución sencilla y reproducción de pistas
    El enemigo de prueba persigue al personaje, y ajusta su Z en función de la distancia (poara que se vean los enemigos más cercanos por encima de los lejanos). También se ha creado el Music Controller que reproduce el tema de cada viñeta

---
+++ Assets/Scripts/Enemy/BullyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BullyMovement : EnemyMovement {
+	public float speed;
+
+	void Start () {
+		base.Initialize ();	
+	}
+
+	void Update () {
+		transform.position = new Vector3 (transform.position.x, transform.position.y, GetZValue ());
+
+		float xVelocity = Mathf.Clamp(speed * (player.position.x - transform.position.x), -speed, speed);
+		rb.velocity = new Vector2 (xVelocity, 0);
+		if (rb.velocity.y < 0) {
+			rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+		}
+	}
+}

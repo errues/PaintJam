3c66f9316dbcf98316d5317d905e617ad1b32447
Samuel Jurado Quintana <samueljurado12@gmail.com>
2018-02-25 23:54:52 +0100

    PauseMenu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PauseMenu : MonoBehaviour {
+
+    public Button ResumeButton;
+    public Button ExitButton;
+    public Image PauseImg;
+
+    private bool paused;
+
+    private void Start() {
+        paused = false;
+        showHideMenu();
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Pause")) {
+            paused = !paused;
+        }
+        showHideMenu();
+        if (paused) {
+            Time.timeScale = 0;
+            
+        } else {
+            Time.timeScale = 1;
+        }
+    }
+
+    private void showHideMenu() {
+        ResumeButton.enabled = paused;
+        ResumeButton.image.enabled = paused;
+        ExitButton.enabled = paused;
+        ExitButton.image.enabled = paused;
+        PauseImg.enabled = paused;
+    }
+
+}

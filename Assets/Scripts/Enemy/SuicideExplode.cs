d39546f927b22233dbef8d6a773577db80999228
errues <errues@gmail.com>
2018-02-25 23:16:51 +0100

    Suicida explota y controles ajustados

--- Assets/Scripts/Enemy/SuicideExplode.cs
+++ Assets/Scripts/Enemy/SuicideExplode.cs
@@ -15,24 +15,24 @@ public class SuicideExplode : MonoBehaviour {
 	private void Start(){
 		audioSource = GetComponent<AudioSource> ();
 		enemySounds = GetComponent<EnemySounds> ();
+		enemyHealth = GetComponent<EnemyHealth> ();
 	}
 
 	public void Explode(){
-		if (enemyHealth != null) {
-			if (!enemyHealth.IsDead ()) {
-				GetComponent<EnemySpriteManager> ().border.sprite = explosionBorder;
-				GetComponent<EnemySpriteManager> ().filling.sprite = explosionFilling;
-				
-				audioSource.PlayOneShot (enemySounds.GetActionClip ());
-				
-				GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezeAll;
-				transform.position = new Vector3 (transform.position.x, transform.position.y, -1);
-				
-				GameObject.FindGameObjectWithTag ("EnemyController").GetComponent<EnemyController> ().EnemyDied ();
-				transform.SetParent (null);
-				
-				Object.Destroy (this.gameObject, explosionTime);
-			}
+		if (!enemyHealth.IsDead ()) {
+			GetComponent<EnemySpriteManager> ().border.sprite = explosionBorder;
+			GetComponent<EnemySpriteManager> ().filling.sprite = explosionFilling;
+			
+			audioSource.PlayOneShot (enemySounds.GetActionClip ());
+			
+			GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezeAll;
+			transform.position = new Vector3 (transform.position.x, transform.position.y, -1);
+			
+			GameObject.FindGameObjectWithTag ("EnemyController").GetComponent<EnemyController> ().EnemyDied ();
+			transform.SetParent (null);
+			
+			Object.Destroy (this.gameObject, explosionTime);
 		}
+
 	}
 }

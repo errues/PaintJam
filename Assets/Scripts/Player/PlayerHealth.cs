e322b0b3fe0392d12147858eaa79263dc125fc92
Samuel Jurado Quintana <samueljurado12@gmail.com>
2018-02-25 11:28:25 +0100

    PlayerMovement reborn

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour {
+    [Range(3,6)]
+    public int life;
+    [Range(3,5)]
+    public float cdInvulnerability;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private bool isInvulnerable;
+
+    private void Start() {
+        isInvulnerable = false;
+    }
+
+    private void Update() {
+        checkDeath();
+    }
+
+    private void checkDeath() {
+        if(life <= 0) {
+            print("ESTAS MUERTO");
+        }
+    }
+
+
+    private void getDamage(int dmg) {
+        life -= dmg;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        
+    }
 }
